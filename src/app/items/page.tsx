c98c5baeb235cfb70d542aa594ef60e4033bc739
Tuguldur515 <102710593+Tuguldur515@users.noreply.github.com>
2023-11-30 16:58:19 +0800

    бараа хайх

---
+++ src/app/items/page.tsx
@@ -0,0 +1,18 @@
+
+const items = () => {
+    return (
+        <>
+        <header className="item search">
+        <h1 className="txtbox">Бараа хайх</h1>
+        </header>
+        <form>
+        <input
+        type="text"
+        className="border border-slate-300 bg-transparent rounded px-2 py-1
+        outline-none focus-within:border-slate-100"
+        />
+        </form>
+        </>
+    )
+}
+export default items;
