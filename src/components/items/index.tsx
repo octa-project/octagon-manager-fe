6f5ffa04ac4ecbdebf44f2e0a7c59b53e602852d
zoljargal olziibat <50563505+zolushka1@users.noreply.github.com>
2023-12-11 08:34:00 +0800

    added  Table

--- src/components/items/index.tsx
+++ src/components/items/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
-import TableCell from "@mui/material/TableCell";
 import { Component } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
@@ -8,30 +7,42 @@ import "ag-grid-community/styles/ag-theme-alpine.css";
 import {
   Alert,
   AlertTitle,
-  Drawer,
   Snackbar,
   TextField,
-  IconButton,
-  ButtonGroup,
   Typography,
   Divider,
   MenuItem,
   Select,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
 } from "@mui/material";
-import { Box } from "@mui/system";
 import api from "@/src/api";
-import items from "@/src/app/items/page";
-import { GroupAdd } from "@mui/icons-material";
 import Image from "next/image";
+import "ag-grid-enterprise";
+import items from '../../app/items/page';
 
 //#region interfaces
 interface Item {
+  id: number;
+  code: string;
+  name: string;
+  measureName: string;
+  itemgroupName: string;
+  createdDate: string;
+  branchId: number;
+  isActive: boolean;
+  isDeleted: boolean;
+  itemcodes: ItemCode[]
+}
+interface ItemCode {
   id: number;
   barcode: string;
   name: string;
   sellPrice: number;
   qty: number;
-  measureName: string;
   createdDate: string;
   isDeleted: boolean;
 }
@@ -52,8 +63,10 @@ interface Measure {
 interface ItemState {
   loading: boolean;
   error: string;
-  selectedRow: Item;
+  selectedItem: Item;
+  selectedItemCode: ItemCode;
   selectedItemGroup: ItemGroup;
+  selectedRow: number;
   isDrawerOpen: boolean;
   drawerType: number;
   open: boolean;
@@ -63,6 +76,7 @@ interface ItemState {
   rowData: Item[];
   measures: Measure[];
   itemGroups: ItemGroup[];
+  selectedRowItemCodes: ItemCode[];
 }
 //#endregion
 
@@ -75,13 +89,24 @@ class ItemController extends Component<{}, ItemState> {
     this.state = {
       loading: false,
       error: '',
-      selectedRow: {
+      selectedItem: {
+        id: 0,
+        code: '',
+        name: '',
+        measureName: "",
+        itemgroupName: "",
+        createdDate: '',
+        isActive: false,
+        isDeleted: false,
+        branchId: 0,
+        itemcodes: []
+      },
+      selectedItemCode: {
         id: 0,
         barcode: '',
         name: '',
         sellPrice: 0,
         qty: 0,
-        measureName: "",
         createdDate: '',
         isDeleted: false,
       },
@@ -94,19 +119,40 @@ class ItemController extends Component<{}, ItemState> {
         isDeleted: false,
         branchId: 0,
       },
+      selectedRow: 0,
       isDrawerOpen: false,
       drawerType: 0,
       open: false,
       columnDefs: [
-        { field: "id", headerName: "№" },
-        { field: "name", headerName: "Нэр", filter: "agTextColumnFilter" },
-        { field: "barcode", headerName: "Баркод", },
-        { field: "sellPrice", headerName: "Зарах үнэ" },
-        { field: "qty", headerName: "Тоо" },
+        {
+          headerName: "Засах",
+          cellRenderer: "buttonRenderer", // Assuming you have a custom buttonRenderer
+        },
+        { field: "id", headerName: "№", width: 50 },
+        { field: "code", headerName: "Баркод" },
+        { field: "name", headerName: "Нэр" },
         { field: "measureName", headerName: "Хэмжих нэгж" },
-        { field: "itemGroup", headerName: "Бүлэг" },
-        { field: "createdDate", headerName: "Бүртгэсэн огноо" },
+        { field: "itemgroupName", headerName: "Бүлэг" },
+        {
+          field: "isActive",
+          headerName: "Идэвхтэй",
+          cellRenderer: "agCheckboxCellRenderer",
+          cellRendererParams: {
+            checkbox: true,
+          },
+          cellStyle: {
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          },
+        },
+        {
+          headerName: "Баркодууд",
+          cellRenderer: "buttonRenderer",
+          label: "aa"
+        },
       ],
+
       defaultColDef: {
         flex: 1,
         minWidth: 100,
@@ -124,6 +170,7 @@ class ItemController extends Component<{}, ItemState> {
       rowData: [],
       measures: [],
       itemGroups: [],
+      selectedRowItemCodes: [],
     };
   }
 
@@ -140,12 +187,25 @@ class ItemController extends Component<{}, ItemState> {
 
   handleTextFieldChange = (field: keyof Item, value: string | number) => {
     this.setState((prevState) => ({
-      selectedRow: {
-        ...prevState.selectedRow,
+      selectedItem: {
+        ...prevState.selectedItem,
         [field]: value,
       },
     }));
   };
+
+  handleRowClick = (row: { id: any; itemcodes?: any[] }) => {
+    this.setState({ selectedRow: row.id });
+    console.log("Selected Row ID:", row.id);
+
+    // Check if itemcodes is defined before logging its length
+    if (row.itemcodes) {
+      console.log("itemcodes:", row.itemcodes.length);
+    } else {
+      console.log("itemcodes is undefined");
+    }
+  };
+
 
   handleTextFieldItemGroupChange = (field: keyof ItemGroup, value: string | number) => {
     this.setState((prevState) => ({
@@ -167,12 +227,27 @@ class ItemController extends Component<{}, ItemState> {
     this.setState({ open: false });
   };
 
-  setItemState = (isOpen: boolean, row: Item, DrawerType: number) => {
+  setItemState = (isOpen: boolean, Item: Item, DrawerType: number) => {
     this.setState({
       isDrawerOpen: isOpen,
-      selectedRow: row,
+      selectedItem: Item,
       drawerType: DrawerType,
     });
+  };
+  setItemCodeState = (isOpen: boolean, ItemCode: ItemCode, DrawerType: number) => {
+    this.setState({
+      isDrawerOpen: isOpen,
+      selectedItemCode: ItemCode,
+      drawerType: DrawerType,
+    });
+  };
+
+  onRowSelected = async (event: { api: { getSelectedRows: () => any; }; }) => {
+    const selectedRows = event.api.getSelectedRows();
+    if (selectedRows.length > 0) {
+      const selectedItem = selectedRows[0];
+      this.setState({ selectedRowItemCodes: selectedItem.itemcodes });
+    }
   };
 
   //#endregion
@@ -219,7 +294,6 @@ class ItemController extends Component<{}, ItemState> {
     try {
       this.setState({ loading: true, error: '' });
 
-      debugger
       const result = await api.measure_get_all.getMeasures();
       if (result.data.code === "200") {
         const measures: Measure[] = result.data.data.map((measure: { id: any; name: any; code: any; }) => ({
@@ -248,14 +322,22 @@ class ItemController extends Component<{}, ItemState> {
       const result = await api.itemCode_get_all_itemcodes.itemCodeGetAllItemCodes();
 
       if (result.data.code === "200") {
-        const rowData: Item[] = result.data.data.map((item: { id: any; barcode: any; name: any; sellPrice: any; qty: any; createdDate: any; isDeleted: string; }) => ({
+        const rowData: Item[] = result.data.data.map((item: {
+          id: any;
+          barcode: any;
+          name: any;
+          sellPrice: any;
+          qty: any;
+          createdDate: any;
+          isDeleted: boolean;
+        }) => ({
           id: item.id,
           barcode: item.barcode,
           name: item.name,
           sellPrice: item.sellPrice,
           qty: item.qty,
           createdDate: item.createdDate,
-          isDeleted: item.isDeleted === 'true',
+          isDeleted: item.isDeleted,
         }));
 
         this.setState({ rowData });
@@ -275,22 +357,33 @@ class ItemController extends Component<{}, ItemState> {
     try {
       this.setState({ loading: true, error: '' });
 
-      const result = await api.itemCode_get_all_itemcodes.itemCodeGetAllItemCodes();
+      const result = await api.item_get_all_items.GetAllItems();
 
       if (result.data.code === "200") {
-        const rowData: Item[] = result.data.data.map((item: { id: any; barcode: any; name: any; sellPrice: any; qty: any; measureName: any; createdDate: any; isDeleted: string; }) => ({
-          id: item.id,
-          barcode: item.barcode,
-          name: item.name,
-          sellPrice: item.sellPrice,
-          qty: item.qty,
-          measureName: item.measureName,
-          createdDate: item.createdDate,
-          isDeleted: item.isDeleted === 'true',
-        }));
+        const rowData: Item[] = result.data.data.map((item: any) => {
+          const { id, code, name, measureName, itemgroupName, isActive, createdDate, itemcodes } = item;
 
-        console.log(result.data.data)
+          return {
+            id,
+            code,
+            name,
+            measureName,
+            itemgroupName,
+            isActive,
+            createdDate,
+            itemcodes: itemcodes.map((itemCode: any) => ({
+              id: itemCode.id,
+              barcode: itemCode.barcode,
+              name: itemCode.name,
+              sellPrice: itemCode.sellPrice,
+              qty: itemCode.qty,
+              createdDate: itemCode.createdDate,
+              isDeleted: itemCode.isDeleted,
+            })),
+          };
+        });
 
+        console.log(result.data.data);
         this.setState({ rowData });
         this.handleClick();
       } else {
@@ -302,6 +395,7 @@ class ItemController extends Component<{}, ItemState> {
       this.setState({ loading: false });
     }
   };
+
 
   deleteItem = async (id: number) => {
     try {
@@ -316,16 +410,16 @@ class ItemController extends Component<{}, ItemState> {
     }
   };
 
-  updateItemCode = async (item: Item | null) => {
+  updateItemCode = async (itemCode: ItemCode | null) => {
     try {
       this.setState({ loading: true, error: '' });
 
       const body = {
-        id: item?.id,
-        barcode: item?.barcode,
-        name: item?.name,
-        sellPrice: item?.sellPrice,
-        qty: item?.qty,
+        id: itemCode?.id,
+        barcode: itemCode?.barcode,
+        name: itemCode?.name,
+        sellPrice: itemCode?.sellPrice,
+        qty: itemCode?.qty,
         isDeleted: false,
         branchId: 1,
       };
@@ -385,22 +479,25 @@ class ItemController extends Component<{}, ItemState> {
       rowData,
       columnDefs,
       defaultColDef,
+      // gridRef,
       autoGroupColumnDef,
-      selectedRow,
+      selectedItem,
+      selectedItemCode,
       selectedItemGroup,
+      selectedRow,
       isDrawerOpen,
       drawerType,
       open,
     } = this.state;
     //#endregion
-   
+
     return (
       <>
         <div className="grid grid-cols-4 gap-3">
 
           <div className="col-span-1 bg-white shadow-md h-screen">
             <Typography className="font-sans text-center font-semibold pt-2 pb-3 text-[#6d758f]">
-              {this.state.selectedRow.id === 0 ? "Шинэ бараа бүртгэх" : "Бараа засах"}
+              {this.state.selectedItem.id === 0 ? "Шинэ бараа бүртгэх" : "Бараа засах"}
             </Typography>
             <Divider className="bg-[#c5cee0] shadow"></Divider>
             <div className="flex flex-col items-center justify-center h-52">
@@ -437,13 +534,13 @@ class ItemController extends Component<{}, ItemState> {
                 <Typography className="font-sans text-left text-xs font-semibold pb-1 text-[#6d758f]">
                   Барааны нэр
                 </Typography>
-                <TextField className="w-full" value={selectedRow?.name} />
+                <TextField className="w-full" value={selectedItem?.name} />
               </div>
               <div className="w-9/12">
                 <Typography className="font-sans text-left text-xs font-semibold pb-1 text-[#6d758f]">
                   Баркод
                 </Typography>
-                <TextField variant="outlined" className="w-full" value={selectedRow?.barcode} />
+                <TextField variant="outlined" className="w-full" value={selectedItem?.code} />
               </div>
               <div className="w-9/12">
                 <Typography className="font-sans text-left text-xs font-semibold pb-1 text-[#6d758f]">
@@ -478,7 +575,9 @@ class ItemController extends Component<{}, ItemState> {
                 <Typography className="font-sans text-left text-xs font-semibold pb-1 text-[#6d758f]">
                   Үнэ
                 </Typography>
-                <TextField variant="outlined" type="number" className="w-full" value={selectedRow?.sellPrice} />
+                <TextField variant="outlined" type="number" className="w-full"
+                // value={selectedItem?.sellPrice} 
+                />
               </div>
               <div className="w-9/12 pt-5">
                 <Button
@@ -491,16 +590,16 @@ class ItemController extends Component<{}, ItemState> {
               <div className="w-9/12">
                 <Button
                   className="font-sans text-[#6d758e] text-base text-center capitalize w-full h-8"
-                  onClick={() => this.setItemState(false, {
-                    id: 0,
-                    barcode: '',
-                    name: '',
-                    sellPrice: 0,
-                    qty: 0,
-                    measureName: "",
-                    createdDate: '',
-                    isDeleted: false,
-                  }, 0)}
+                // onClick={() => this.setItemState(false, {
+                //   id: 0,
+                //   code: '',
+                //   name: '',
+                //   sellPrice: 0,
+                //   qty: 0,
+                //   measureName: "",
+                //   createdDate: '',
+                //   isDeleted: false,
+                // }, 0)}
                 >
                   Болих
                 </Button>
@@ -516,10 +615,70 @@ class ItemController extends Component<{}, ItemState> {
               </div>
             </div>
 
-            <div className="bg-white flex-initial w-full h-4/5 p-2">
+            <div className="bg-white flex-initial w-full h-4/5 shadow-lg rounded-lg">
               <div style={containerStyle}>
-                <div className="ag-theme-alpine" style={gridStyle}>
+                <Table className="font-sans">
+                  <TableHead className="" >
+                    <TableRow className="bg-[#8a91a5]">
+                      <TableCell className="font-sans text-white "></TableCell>
+                      <TableCell className="font-sans text-white ">Засах</TableCell>
+                      <TableCell className="font-sans text-white ">№</TableCell>
+                      <TableCell className="font-sans text-white ">Код</TableCell>
+                      <TableCell className="font-sans text-white ">Нэр</TableCell>
+                      <TableCell className="font-sans text-white ">Хэмжих нэгж</TableCell>
+                      <TableCell className="font-sans text-white ">Бүлэг</TableCell>
+                      <TableCell className="font-sans text-white ">Идэвхгүй</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {rowData.map((row) => (
+                      <React.Fragment key={row.id}>
+                        <TableRow>
+                          <TableCell>
+                            <div>
+                              <Button
+                                onClick={() => this.handleRowClick(row)}
+                                className="bg-[#8a91a5] w text-white hover:bg-black"
+                              >
+                                {selectedRow === row.id ? 'V' : '>'}
+                              </Button>
+                            </div>
+                          </TableCell>
+                          <TableCell>
+                            <div>
+                              <Button className="bg-[#8a91a5] text-white hover:bg-black">
+                                Засах
+                              </Button>
+                            </div>
+                          </TableCell>
+                          <TableCell className="font-sans">{row.id}</TableCell>
+                          <TableCell className="font-sans">{row.code}</TableCell>
+                          <TableCell className="font-sans">{row.name}</TableCell>
+                          <TableCell className="font-sans">{row.measureName}</TableCell>
+                          <TableCell className="font-sans">{row.itemgroupName}</TableCell>
+                          <TableCell className="font-sans">{row.isActive}</TableCell>
+                        </TableRow>
+                        {selectedRow === row.id && row.itemcodes && row.itemcodes.length > 0 && (
+                          row.itemcodes.map((itemCode) => (
+                            <TableRow key={itemCode.id}>
+                              <TableCell>{itemCode.name}</TableCell>
+                              <TableCell>{itemCode.barcode}</TableCell>
+                              <TableCell>{itemCode.sellPrice}</TableCell>
+                              <TableCell>{itemCode.qty}</TableCell>
+                              <TableCell>{itemCode.createdDate}</TableCell>
+                            </TableRow>
+                          ))
+                        )}
+                      </React.Fragment>
+                    ))}
+                  </TableBody>
+                </Table>
+
+
+
+                {/* <div className="ag-theme-alpine" style={gridStyle}>
                   <AgGridReact
+                    // ref={gridRef}
                     rowData={rowData}
                     columnDefs={columnDefs}
                     animateRows={true}
@@ -535,15 +694,21 @@ class ItemController extends Component<{}, ItemState> {
                       const itemData: Item = e.data as Item;
                       this.setItemState(false, itemData, 0);
                     }}
+                  // onRowDoubleClicked={(e) => {
+                  //   const itemData = e.data as Item;
+                  //   this.setState({ selectedItem: itemData, subitems: itemData.itemcodes });
+                  //   this.setItemState(false, itemData, 0);
+                  // }}
                   />
-                </div>
+                </div> */}
+
               </div>
             </div>
           </div>
         </div>
 
 
-        <Drawer
+        {/* <Drawer
           anchor="left"
           open={isDrawerOpen && drawerType == 0}
           onClose={() => this.setItemState(false, {
@@ -658,7 +823,7 @@ class ItemController extends Component<{}, ItemState> {
               Бүлэг хадгалах
             </Button>
           </Box>
-        </Drawer>
+        </Drawer> */}
         <Snackbar
           open={open}
           autoHideDuration={6000}
