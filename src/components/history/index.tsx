ad956b930459e94fa6ef07a0f5a73e9141c87319
Tuguldur515 <102710593+Tuguldur515@users.noreply.github.com>
2023-12-13 14:47:45 +0800

    added history change

--- src/components/history/index.tsx
+++ src/components/history/index.tsx
@@ -1,7 +1,8 @@
 "use client"
-import { Button, Divider, TextField, Tooltip } from "@mui/material";
+import { Button, Divider, Input, TextField, Tooltip } from "@mui/material";
 import { Component } from "react";
 import SaleHistory from "./saleHistory";
+import SaleReport from "../reports/sale";
 
 class HistoryController extends Component {
     state = {
@@ -12,51 +13,86 @@ class HistoryController extends Component {
     };
 
   render() {
-    const normal = 'font-sans text-base rounded-md w-full h-12 capitalize text-white bg-slate-400 hover:bg-slate-200 text-black';
-    const hovered = 'font-sans text-base rounded-md w-full h-12 capitalize text-black bg-slate-50 hover:bg-slate-200';
+    const normal = 'font-sans text-sm rounded-2xl shadow w-full h-12 text-white bg-slate-400 hover:bg-slate-200 text-black';
+    const hovered = 'font-sans text-sm rounded-2xl shadow w-full h-12 text-black bg-slate-50 hover:bg-slate-200';
 
     return (
-        <div className="flex">
-            <div className='bg-white flex-initial w-56 h-autp'>
-                <div className="flex-initial">
-                    <ul className='py-2'>
-                        <li className='py-2 px-4 pt-10'>
-                            <Tooltip title="Борлуулалын түүх" placement="left">
-                                <Button
-                                    onClick={() => this.changeReport(0)}
-                                    className={`${this.state.historyIndex === 0
-                                        ? normal
-                                        : hovered}`}
-                                >Борлуулалын түүх</Button>
-                            </Tooltip>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <Divider orientation="vertical" variant="middle" />
-            <div className="flex-initial w-full h-screen">
+        <div className="grid grid-cols-5 gap-3">
+        <div className='col-span-1 bg-white shadow-md h-screen'>
+            <div className="flex-initial">
+                <ul className='py-2'>
+                    <li className='py-2 px-4 pt-10'>
+                        <Button
+                            onClick={() => this.changeReport(0)}
+                            className={`${this.state.historyIndex === 0
+                                ? normal
+                                : hovered}`}>
+                            Борлуулалтын түүх</Button>
+                    </li>
+                    {/* <li className='py-2 px-4 pb-10'>
+                        <Button
+                            onClick={() => this.changeReport(1)}
+                            className={`${this.state.historyIndex === 1
+                                ? normal
+                                : hovered}`}>
+                            Зарлага</Button>
+                    </li>
+                    <Divider variant="middle" />
+                    <li className='py-2 px-4 pt-10'>
 
-                {/* <div className="bg-white p-2 flex-initial w-full ">
-                    <div className="grid grid-cols-3 gap-5">
-                        <div className="col-span-1">
-                            <h1 >Хайх</h1>
-                            <TextField className="" variant="standard" />
-                        </div>
-                        <div className="col-span-2">
-                           
-                        </div>
-                    </div>
-                </div> */}
-                <div className="flex-initial w-full h-screen">
-                    {this.state.historyIndex === 0 ? <SaleHistory /> : null}
-                    {/* {this.state.reportIndex === 1 ? <OutComeReport /> : null}
-                    {this.state.reportIndex === 2 ? <ItemPriceReport /> : null}
-                    {this.state.reportIndex === 3 ? <SaleReport /> : null}
-                    {this.state.reportIndex === 4 ? <CashierReport /> : null} */}
-                </div>
+                        <Button
+                            onClick={() => this.changeReport(2)}
+                            className={`${this.state.historyIndex === 2
+                                ? normal
+                                : hovered}`}>
+                            Үнийн түүх</Button>
+                    </li>
+                    <li className='py-2 px-4 pb-10'>
+                        <Button
+                            onClick={() => this.changeReport(3)}
+                            className={`${this.state.historyIndex === 3
+                                ? normal
+                                : hovered}`}>
+                            Борлуулалт</Button>
+                    </li>
+                    <Divider variant="middle" />
+                    <li className='py-2 px-4 pt-10'>
+                        <Button
+                            onClick={() => this.changeReport(4)}
+                            className={`${this.state.historyIndex === 4
+                                ? normal
+                                : hovered}`}>
+                            Касс</Button>
+                    </li> */}
+                </ul>
             </div>
         </div>
-    )
+        <div className="flex flex-col col-span-4">
+            <div className="flex h-24 p-3">
+                <div className="flex items-center bg-white h-14 w-full rounded-2xl shadow border border-[#cbcbcb]">
+                    <Input
+                        className="capitalize text-[#6d758f] w-full h-full rounded-2xl border-none pl-3 pr-8"
+                        placeholder="Хайх..."
+                    />
+                    {/* <Image
+                        src="/items/search.svg"
+                        alt="icon"
+                        width={24}
+                        height={24}
+                        className="mr-5 cursor-pointer"
+                    /> */}
+                </div>
+            </div>
+            <div className="h-full p-3">
+                {this.state.historyIndex === 0 ? <SaleHistory /> : null}
+                {/* {this.state.historyIndex === 1 ? <OutComeReport /> : null}
+                {this.state.historyIndex === 2 ? <ItemPriceReport /> : null}
+                {this.state.historyIndex === 3 ? <SaleReport /> : null}
+                {this.state.historyIndex === 4 ? <CashierReport /> : null} */}
+            </div>
+        </div>
+    </div>
+)
 }
 }
 export default HistoryController;
