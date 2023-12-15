fa8060c17f4853fe55fef85234f57265047aa2e5
zoljargal olziibat <50563505+zolushka1@users.noreply.github.com>
2023-12-15 11:01:14 +0800

    added

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
+    // output: 'export',
+    // reactStrictMode: true,
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+    
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+    
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+    eslint: { 
+        ignoreDuringBuilds: true
+    },
     env: {
         api_url: '43.231.113.215'
     }
 }
-
 module.exports = nextConfig
