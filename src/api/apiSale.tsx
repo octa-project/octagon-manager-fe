3a101db4d3d6e9fbc0067c79bd68343b7ede9790
zoljargal olziibat <50563505+zolushka1@users.noreply.github.com>
2023-12-18 10:07:05 +0800

    added url

--- src/api/apiSale.tsx
+++ src/api/apiSale.tsx
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const URL_PREFIX = process.env.REACT_APP_IS_PROD ? process.env.REACT_APP_PROD_URL_PREFIX : "http://43.231.114.215:7400";
+export const URL_PREFIX = process.env.REACT_APP_IS_PROD ? process.env.REACT_APP_PROD_URL_PREFIX : "http://43.231.114.215:7001";
 
 const apiSale = (mainRouteName: string) => ({
     GetMany:(startDate:any,endDate:any) => axios.get(`${URL_PREFIX}/sale/${mainRouteName}?startDate=${startDate}&endDate=${endDate}`),
