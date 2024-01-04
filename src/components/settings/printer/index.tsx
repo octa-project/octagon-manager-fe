7df2163aa73d4419351d798363667f8d2d03eb67
zoljargal olziibat <50563505+zolushka1@users.noreply.github.com>
2024-01-04 14:56:12 +0800

    fiex tab bar class

--- src/components/settings/printer/index.tsx
+++ src/components/settings/printer/index.tsx
@@ -1,16 +1,14 @@
-import {Component} from "react";
+import { Component } from "react";
 
 
 class PrinterSettings extends Component<{}, AgReportState>{
     render() {
-        const containerStyle = { width: '100%', height: '100%' };
-        return(
+        return (
 
             <div className="h-full">
-
-                <div style={containerStyle}>
-                    sdasdasdasds
-                </div>
+                    <div>
+                        
+                    </div>
             </div>
         );
     }
