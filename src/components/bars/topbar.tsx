0db10cb06dc85154f23d6d004a3a8de5a51ea1ff
zoljargal olziibat <50563505+zolushka1@users.noreply.github.com>
2024-01-05 16:20:09 +0800

    added header

--- src/components/bars/topbar.tsx
+++ src/components/bars/topbar.tsx
@@ -1,8 +1,8 @@
 import Avatar from "@mui/material/Avatar"
-import {LogoutOutlined, PersonOutlineOutlined} from '@mui/icons-material'
-import {IconButton, Link} from "@mui/material"
+import { LogoutOutlined, PersonOutlineOutlined } from '@mui/icons-material'
+import { Button, IconButton, Link } from "@mui/material"
 import moment from "moment"
-import {Component, useEffect, useState} from "react";
+import { Component, useEffect, useState } from "react";
 import Image from "next/image";
 
 const Topbar = () => {
@@ -15,39 +15,28 @@ const Topbar = () => {
         return () => clearInterval(intervalId);
     }, []);
 
-    const formattedDateTime = currentDateTime.toLocaleString();
+    const formattedDateTime = moment(currentDateTime).format("YYYY-MM-DD HH:mm:ss");
     return (
-        <div className='flex flex-row bg-white text-indigo-800 p-3'>
-            <span className="text-[#6D758F] mr-2">Demo manager</span>
+        <div className='flex-row w-full bg-[#6d758f] p-3 opacity-80'>
+            <div className="grid grid-cols-5">
+                <div className="headerText col-span-2">Demo manager</div>
+                <div className="headerText col-span-2">{formattedDateTime}</div>
 
-                <p className="ml-10">Current Time: {formattedDateTime}</p>
-
-            <div className="flex flex-row justify-end ml-80">
-                <Image className=" ml-80 " src="/items/sales.svg" alt="SVG Image" width={120} height={60}></Image>
-                <Image className="ml-5 mr-5" src="/items/logout.svg" alt="SVG Image" width={20} height={20}></Image>
-                <Image className=" mr-20 items-end" src="/items/notification-bell.svg" alt="SVG Image" width={20} height={20}></Image>
-
+                <div className="col-span-1">
+                    <div className="flex flex-row justify-end gap-6">
+                        <Button className="headerButton flex items-center">
+                            <span className="mr-2">Борлуулалт</span>
+                            <Image src="/header/sale.svg" alt="SVG Image" width={20} height={20} />
+                        </Button>
+                        <Button className="headerButton">
+                            <Image src="/header/bell.svg" alt="SVG Image" width={20} height={20} />
+                        </Button>
+                        <Button className="headerButton ml-auto"> {/* Set margin-left to auto */}
+                            <Image src="/header/signOut.svg" alt="SVG Image" width={20} height={20} />
+                        </Button>
+                    </div>
+                </div>
             </div>
-
-            {/*<div>*/}
-            {/*    <div className='flex flex-row gap-3'>*/}
-
-            {/*        <strong className='leading-none'>Болорчимэг</strong>*/}
-
-            {/*    </div>*/}
-            {/*    <div className='text-center py-[5px]'>*/}
-            {/*        <span className='text-gray-500'>Manager:</span> <strong>Салбар 1</strong>*/}
-            {/*    </div>*/}
-            {/*    <div className='flex flex-row items-center gap-3 mr-1'>*/}
-            {/*        <div className='flex flex-row'>{moment().format("YYYY/MM/DD H:m")}</div>*/}
-            {/*        <Link href="http://localhost:3001" >*/}
-            {/*            <IconButton className="rounded-lg bg-gray-100 text-gray-500 hover:bg-indigo-500 hover:text-white"><PersonOutlineOutlined sx={{ fontSize: 18 }} /></IconButton>*/}
-            {/*        </Link>*/}
-            {/*        <IconButton className="rounded-lg bg-gray-100 text-gray-500 hover:bg-rose-500 hover:text-white mr-5"><LogoutOutlined sx={{ fontSize: 18 }} /></IconButton>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-
-
         </div>
     )
 }
