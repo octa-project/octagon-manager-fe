1171c0afdd45974172974771641f7281ed29b6fc
temuulen <temuulen.n@mcs.mn>
2024-01-05 15:38:01 +0800

    main

--- src/components/bars/sidebar.tsx
+++ src/components/bars/sidebar.tsx
@@ -37,6 +37,16 @@ const Sidebar: React.FC<SidebarProps> = ({ collapsed, toggleSidebar }) => {
             width={collapsed ? 40 : 150} height={collapsed ? 40 : 40} />
         </div>
         <div className='flex flex-col justify-start items-center'>
+          <div className="flex flex-row w-full items-center ">
+            <Button className="bg-white ml-2 mr-2 flex flex-row items-center rounded-b w-full">
+              <Image className="ml-4 " src="/sidebar/defaultProfilePic.png" alt="profile picture" width={50} height={50}></Image>
+             <div className="flex flex-col items-center">
+               <p className="justify-end mr-16 ml-10 text-#6D758F">Name</p>
+               <p className="justify-end mr-16 ml-10 text-#6D758FB2 text-xs">role</p>
+             </div>
+              <Image className="justify-end mr-5" src="/sidebar/downArrow.svg" alt="SVG Image" width={20} height={20}></Image>
+            </Button>
+          </div>
           {sideBarItems.map((item, index) => (
             <div key={index} className="p-2 w-full">
               <Tooltip title={collapsed ? item.name : ''} arrow placement={collapsed ? 'right' : 'right-end'}>
